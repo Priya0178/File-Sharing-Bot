e295a4d8623dee1c870386e8eacc277e292c0d9e
SONU KANWAR <111267360+Priya0178@users.noreply.github.com>
2024-10-14 00:56:44 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -104,8 +104,8 @@ async def start_command(client: Client, message: Message):
             pass'''
         return
     else:
-        '''reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🥵فضيحة هدير كامله", url=f'https://t.me/VideosIMUBOT')],[InlineKeyboardButton("🔥🔞قناة الفضيحة", url=f'https://t.me/VideosIMUBOT')]]
-        )'''
+        reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🥵فضيحة هدير كامله", url=f'https://t.me/VideosIMUBOT')],[InlineKeyboardButton("🔥🔞قناة الفضيحة", url=f'https://t.me/VideosIMUBOT')]]
+        )
         await message.reply_text(
             text = START_MSG.format(
                 first = message.from_user.first_name,
